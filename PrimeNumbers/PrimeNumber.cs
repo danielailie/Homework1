9dd335ed8786d53fec7f8adc0204be406c7d4b6c
DanielaIlie <danielailie12@gmail.com>
2015-03-10 12:01:44 +0200

    Second Exercise Added

---
+++ PrimeNumbers/PrimeNumber.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace PrimeNumbers
+{
+    class PrimeNumber
+    {
+        public int NumberOfNumbers { get; set; }
+
+        public PrimeNumber(int n)
+        {
+            NumberOfNumbers = n;
+        }
+
+        public void PrintTheArrayOfNumbers()
+        {
+            for (int i = 1; i <= NumberOfNumbers; i++)
+            {
+                if (isPrime(i))
+                    Console.Write(i + "-prime, ");
+                else
+                {
+                    Console.Write( i + ", ");
+                }
+            }
+            Console.ReadLine();
+        }
+
+        private static bool isPrime(int n)
+        {
+            if (n <= 1)
+                return false;
+            if (n%2 == 0)
+                return false;
+            for (int i = 2; i <= n / 2; i++)
+                if (n % i == 0)
+                    return false;
+            return true;
+        }
+    }
+}
---
+++ PrimeNumbers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PrimeNumbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            var numberOfNumbers = Convert.ToInt32(Console.ReadLine());
+
+            var primeNumber=new PrimeNumber(numberOfNumbers);
+            primeNumber.PrintTheArrayOfNumbers();
+        }
+
+    }
+}
